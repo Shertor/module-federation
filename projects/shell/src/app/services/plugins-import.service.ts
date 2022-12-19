30bf1567b0b070b90c0ad41821ecb3305ba89264
Shertor <shertors@gmail.com>
2022-12-19 17:03:30 +0300

    shell: plugins-import service update

--- projects/shell/src/app/services/plugins-import.service.ts
+++ projects/shell/src/app/services/plugins-import.service.ts
@@ -1,9 +1,57 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { LookupService } from "../utils/plugins/lookup.service";
+import { PluginOptions } from "../utils/plugins/plugin";
 
+/**
+ * Сервис для загрузки и хранения манифеста с плагинами
+ */
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-export class PluginsImportService {
+export class PluginsImportService implements OnInit {
+  /** Флаг состояния загрузки плагинов */
+  public loading$: BehaviorSubject<boolean> = new BehaviorSubject(true);
+  /** Список всех плагинов из манифеста*/
+  private plugins: PluginOptions[] = [];
 
-  constructor() { }
+  constructor(private lookupService: LookupService) {}
+
+  ngOnInit(): void {
+    this.loadManifest();
+  }
+
+  /** Функция для загрузки манифеста */
+  private loadManifest(): void {
+    if (this.loading) return;
+
+    this.loading$.next(true);
+    this.lookupService.lookup().then((plugins) => {
+      this.loading$.next(false);
+      this.plugins = plugins;
+    });
+  }
+
+  /** Функция для обновления манифеста */
+  public update(): void {
+    this.loadManifest();
+  }
+
+  /** Геттер для флага состояния загрузки плагинов*/
+  public get loading(): boolean {
+    return this.loading$.getValue();
+  }
+
+  /**
+   * Возвращает список всех плагинов, или список плагинов для выбранной страницы
+   * @param page Номер страницы, на которой необходимо расположить плагин
+   * @returns Список плагинов
+   */
+  public getPlugins(page?: number | undefined): PluginOptions[] {
+    if (page) {
+      return this.plugins.filter((p) => (p.page ? p.page === page : false));
+    }
+
+    return this.plugins;
+  }
 }
--- projects/shell/src/app/utils/plugins/plugin.ts
+++ projects/shell/src/app/utils/plugins/plugin.ts
@@ -3,4 +3,6 @@ import { LoadRemoteModuleOptions } from "@angular-architects/module-federation";
 export type PluginOptions = LoadRemoteModuleOptions & {
   unicDisplayName: string;
   componentName: string;
+
+  page: number | undefined;
 };
