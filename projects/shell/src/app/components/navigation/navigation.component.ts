844015beda8fe5c0eec4728865135931429c7312
Shertor <shertors@gmail.com>
2022-12-19 17:56:05 +0300

    shell: navigation template

--- projects/shell/src/app/components/navigation/navigation.component.ts
+++ projects/shell/src/app/components/navigation/navigation.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ModulesImportService } from "../../services/modules-import.service";
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  selector: "app-navigation",
+  templateUrl: "./navigation.component.html",
+  styleUrls: ["./navigation.component.scss"],
 })
 export class NavigationComponent {
-
+  constructor(public modulesService: ModulesImportService) {}
 }
